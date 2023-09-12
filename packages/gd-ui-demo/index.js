4e1d16326a6b5ae1e39ad24c466f7f52bdb4492a
gzdl-strive <228419172@qq.com>
2023-09-12 15:47:58 +0800

    feat: gd-ui-demo to verify sub-package

---
+++ packages/gd-ui-demo/index.js
@@ -0,0 +1,13 @@
+import { createApp } from "vue/dist/vue.esm-bundler.js";
+import GdUI, { JSXButton, SFCButton } from "gd-ui-vite/dist/gd-ui.mjs";
+import "gd-ui-vite/dist/assets/css/entry.css";
+
+createApp({
+  template: `
+    <JSXButton></JSXButton>
+    <br />
+    <br />
+    <GButton color="purple">111</GButton>
+    <GInput placeholder="用户名" clearable />
+  `
+}).use(GdUI).mount("#app");
